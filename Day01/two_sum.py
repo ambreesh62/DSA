3d4d2a965cf17ce347092cc1c576c67621283303
Bikram <ambreeshpatel98@gmail.com>
2024-07-05 19:23:42 +0530

    two Sum solved

--- Day01/two_sum.py
+++ Day01/two_sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def two_sum(self, nums, target):
+    def twoSum(self, nums, target):
         num_to_index = {}
         
         for i, num in enumerate(nums):
@@ -10,6 +10,6 @@ class Solution:
 
 solution = Solution()
 
-print(solution.two_sum([2, 7, 11, 15], 9))  
-print(solution.two_sum([3, 2, 4], 6))      
-print(solution.two_sum([3, 3], 6))        
+print(solution.twoSum([2, 7, 11, 15], 9))  
+print(solution.twoSum([3, 2, 4], 6))      
+print(solution.twoSum([3, 3], 6))        
